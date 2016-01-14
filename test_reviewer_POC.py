b608dece755f21a562aaabffd3d476a744b4acb8
Innes Morrison <innesdmorrison@gmail.com>
2016-01-14 15:50:32 +0000

    Added Jupyter notebook for running summary analysis on GCSE mock results

--- test_reviewer_POC.py
+++ test_reviewer_POC.py
@@ -72,7 +72,7 @@ class Reviewer():
             normRes = result(r.UPN, r.tSet, r.aID, r.qNum, float("{0:.5f}".format(norm)))
             self.active_results.append(normRes)
 
-    def select(self, mode='worst', numPerPupil=3):
+    def select(self, mode='worst', numPerPupil=3, overall_performance=False):
         '''
         For each pupil and each assessment, identify their best/worst questions.
         Return is a dict of {aID:
@@ -87,13 +87,15 @@ class Reviewer():
             UPNs = DB.execute(query, (self.teaching_set,)).fetchall()
 
         classPerformance = dict()
+        titleDict = dict()
         for aID in self.assessmentIDs:
+            titleDict[aID] = dict()
             with sqlite3.connect(self.DBname) as DB:
                 query = "SELECT qNum, qTitle FROM assessments where aID = ?"
                 qTitles = DB.execute(query, (aID,)).fetchall()
-                titleDict = dict()
                 for q in qTitles:
-                    titleDict[q[0]] = q[1]
+                    titleDict[aID][q[0]] = q[1]
+
             pupilPerformance = dict()
             for UPN in UPNs:
                 performance = [r for r in self.active_results if r.aID == aID and r.UPN == UPN[0]]
@@ -102,78 +104,114 @@ class Reviewer():
                     perf = performance[:numPerPupil]
                 else:
                     perf = performance[-numPerPupil:]
-                readable_results = [(titleDict[r.qNum], r.norm) for r in perf]
+                readable_results = [(titleDict[r.aID][r.qNum], r.norm) for r in perf]
 
                 with sqlite3.connect(self.DBname) as DB:
                     query = "SELECT Name FROM cohort WHERE UPN = ?"
                     name = DB.execute(query, (UPN[0],)).fetchone()[0]
 
                 pupilPerformance[name] = readable_results
-            # -- OrderedDict to maintain register order of pupils
-            classPerformance[aID] = OrderedDict(sorted(pupilPerformance.items()))
+                # -- OrderedDict to maintain register order of pupils
+                classPerformance[aID] = OrderedDict(sorted(pupilPerformance.items()))
 
         self.output = classPerformance
+        if overall_performance:
+            self.output = self.calc_overall_performance(mode, numPerPupil)
 
-    def write_to_txt(self, filename="output.txt"):
+    def calc_overall_performance(self, mode, numPerPupil):
+        ''' Find the best/worst questions for all assessments'''
+        all_results = dict()
+        for aID, results in self.output.items():
+            for pupil, performance in results.items():
+                if pupil not in all_results:
+                    all_results[pupil] = performance
+                else:
+                    for p in performance:
+                        all_results[pupil].append(p)
+
+        sortedRes = OrderedDict(sorted(all_results.items()))
+        for pupil in sortedRes:
+            sortedRes[pupil] = sorted(sortedRes[pupil], key=lambda x: x[1])
+            QWC_found = False
+            for i, q in enumerate(sortedRes[pupil]):
+                if 'QWC' in q[0] and not QWC_found:
+                    # Rename QWC questions as targets
+                    sortedRes[pupil][i] = ('QWC: Identify the problem topic and select an appropriate strategy', q[1])
+                    QWC_found = True
+                elif 'QWC' in q[0] and QWC_found:
+                    # Remove the duplicate
+                    sortedRes[pupil].pop(i)
+
+            if mode == 'worst':
+                sortedRes[pupil] = sortedRes[pupil][:numPerPupil]
+            else:
+                sortedRes[pupil] = sortedRes[pupil][-numPerPupil:]
+
+        return sortedRes
+
+    def write_to_txt(self, filename="output.txt", allAssessments=True):
         with open(filename, "w") as f:
-            f.write("Here are the lowest performing topics per pupil vs the class mean.\n"
-                    "Normalised performance has been calculated as:\n"
-                    "  norm = (pupil score - class mean) / question total\n\n")
-            for assessment in self.output:
-                with sqlite3.connect(self.DBname) as DB:
-                    query = "SELECT aName FROM assessments WHERE aID = ?"
-                    aName = DB.execute(query, (assessment,)).fetchone()[0]
-                f.write("Assessment: {}".format(aName))
-                for pupil in self.output[assessment]:
-                    f.write('\n ' + pupil + ':\n')
-                    for Q in self.output[assessment][pupil]:
-                        f.write("  {}: ({})\n".format(Q[0].strip(), Q[1]))
-                f.write('\n\n')
+            f.write("Name,Question 1,Question 2,Question 3")
+            if not allAssessments:
+                for assessment in self.output:
+                    with sqlite3.connect(self.DBname) as DB:
+                        query = "SELECT aName FROM assessments WHERE aID = ?"
+                        aName = DB.execute(query, (assessment,)).fetchone()[0]
+                    f.write("Assessment: {}".format(aName))
+                    for pupil in self.output[assessment]:
+                        name = pupil.split()
+                        f.write('\n' + name[1] + ' ' + name[0] + '\n')
+                        for Q in self.output[assessment][pupil]:
+                            # .xlsx is a CSV format so get rid of any commmas!
+                            cleanedTopic = Q[0].replace(',', '').replace(';', '')
+                            # Also remove any tabs or newlines to protect formatting
+                            f.write("{},".format(cleanedTopic.strip()))
+                    f.write('\n')
+            else:
+                for pupil in self.output:
+                    name = pupil.split()
+                    f.write('\n' + name[1] + ' ' + name[0])
+                    for Q in self.output[pupil]:
+                        # .xlsx is a CSV format so get rid of any commmas!
+                        cleanedTopic = Q[0].replace(',', '').replace(';', '')
+                        # Also remove any tabs or newlines to protect formatting
+                        f.write("{},".format(cleanedTopic.strip()))
 
-    def write_to_xlsx(self, filename="output.xlsx"):
-        # This uses openpyxl
+    def write_to_xlsx(self, filename="output.xlsx", allAssessments=True):
         wb = Workbook()
         # This will create a new sheet --> ws = wb.create_sheet()
         ws = wb.active
-        ws.title = "ClMATE Assessment analysis - " + self.teaching_set
-
-        ''' This is from the data entry screen - REWRITE! '''
-        # -- Set up column headings
-        headings = ['UPN', 'Pupil name', 'Target']
-        for n in range(NUM_QUESTIONS):
-            headings.append(str(n + 1))
-        headings.append('Total')
-        headings.append('  %  ')
-        headings.append('Grade')
-        headings.append('Relative')
-        for col in range(NUM_QUESTIONS + 7):
-            heading = ws.cell(row=1, column=(col + 1))
+        # Set headings
+        headings = ["Name", "Question 1", "Question 2", "Question 3"]
+        for col in range(4):
+            heading = ws.cell(row=0, column=(col))
             heading.value = headings[col]
 
-        # -- Rip data
-        DBname = self.session_details["DBname"]
-        with sqlite3.connect(DBname) as DB:
-            for r in range(self.CLASS_SIZE):
-                upn_val = DB.execute("select UPN from cohort where name = ?",
-                                     (self.name_list[r],)).fetchone()[0]
-                upn = ws.cell(row=(r + 2), column=1)
-                upn.value = upn_val
-                for c in range(NUM_QUESTIONS + 5):
-                    cell_contents = str(questions.item(r, c).text())
-                    excel_cell = ws.cell(row=(r + 2), column=(c + 2))
-                    excel_cell.value = cell_contents
-                rel = str(questions.item(r, NUM_QUESTIONS + 5).text()[1:])
-                excel_cell = ws.cell(row=(r + 2), column=(NUM_QUESTIONS + 7))
-                excel_cell.value = rel
-        DB.close()
+        if not allAssessments:
+            print("Idividual assessments not yet implemented! Try again...")
+        else:
+            for row, pupil in enumerate(self.output):
+                name = pupil.split()
+                nameCell = ws.cell(row=row + 1, column=(0))
+                nameCell.value = name[1] + ' ' + name[0].replace(',', '')
 
-        wb.save(title)
-
+                for col, Q in enumerate(self.output[pupil]):
+                    # .xlsx is a CSV format so get rid of any commmas and semicolons
+                    cleanedTopic = Q[0].replace(',', '').replace(';', '')
+                    # Also remove any tabs or newlines to protect formatting
+                    questionCell = ws.cell(row=row + 1, column=(col + 1))
+                    questionCell.value = "{}".format(cleanedTopic.strip())
+        wb.save(filename)
 
 if __name__ == '__main__':
+    '''
+    Generate the target stickers for each class and find the lowest performing questions
+    per tier.
+    '''
     y11 = ['11-PG', '11-DBY', '11-HH', '11-CCL', '11-AR', '11-AK', '11-IM', '11-MD']
+
     for tSet in y11:
         r = Reviewer(teaching_set=tSet, DBname="ClMATE_DB.db")
         r.normalise(against='class')
-        r.select(mode='worst', numPerPupil=3)
-        r.write_to_txt(tSet + " - worst 3.txt")
+        r.select(mode='worst', numPerPupil=3, overall_performance=True)
+        r.write_to_xlsx(tSet + " - target labels.xlsx")
